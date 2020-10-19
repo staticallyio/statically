9e2ae090fafc3b802486560ef011ec88a80ddad9
Frans Allen <git.a@upset.dev>
2020-10-19 17:21:30 +0700

    Change donation link text

--- src/components/header.js
+++ src/components/header.js
@@ -89,12 +89,11 @@ function Header({ siteTitle }) {
         <div className="flex mt-3 lg:mt-0">
           <a
             href="https://www.patreon.com/fransallen"
-            className="btn btn-primary btn-sm umami--click--btn-become-a-sponsor"
-            title="Become A Backer"
+            className="btn btn-primary btn-sm umami--click--btn-donate-1-dollar"
             rel="noopener noreferrer"
             target="_blank"
           >
-            Become A Sponsor
+            Donate $1 via Patreon
           </a>
         </div>
       </div>
